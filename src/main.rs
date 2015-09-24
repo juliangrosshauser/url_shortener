89e942cd62706297fbf2f6e02534a1f1c31699c7
Julian Grosshauser <julian@juliangrosshauser.com>
2015-09-25 01:16:26 +0200

    Add `connect()` function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate url;
+extern crate postgres;
 
 use std::env;
 use url::Url;
+use postgres::{Connection, SslMode};
+use postgres::error::ConnectError;
 
 const POSTGRES_HOST: &'static str = env!("POSTGRES_HOST");
 const POSTGRES_PORT: &'static str = env!("POSTGRES_PORT");
@@ -33,4 +36,13 @@ fn main() {
     }
 
     let _url = url.unwrap();
+}
+
+fn connect() -> Result<Connection, ConnectError> {
+    let url = format!("postgres://{user}:{password}@{host}:{port}/{database}",
+                        user = POSTGRES_USER, password = POSTGRES_PASSWORD,
+                        host = POSTGRES_HOST, port = POSTGRES_PORT,
+                        database = POSTGRES_DATABASE);
+
+    return Connection::connect(&*url, &SslMode::None);
 }
